845ad514bd913bfa352b7977d024eec7024164c1
Alexis Curisaca <alexis.curisaca25@trinityschoolnyc.org>
2024-04-07 18:37:46 -0400

    attempted making json of csv, didnt work :(

--- Data_Journalism/data/format_data.py
+++ Data_Journalism/data/format_data.py
@@ -1,17 +1,22 @@
 import json
+import csv
 
 
-f1 = open("Parks_Data.csv", "r")
+f1 = open("Data_Journalism/data/Parks_Data.csv", "r")
 lines = f1.readlines()
-
-dictionary ={}
+list=[]
+dictionary={}
+with open('Data_Journalism/data/Parks_Data.csv') as f:
+   for line in csv.DictReader(f, fieldnames=('ACRES','BOROUGH','LOCATION','SIGNNAME','TYPECATEGORY','URL','ZIPCODE','multipolygon')):
+    list.append(line['SIGNNAME'])
+print(list)
 
 # Create the dictionary here
 
 f1.close()
 
 #Save the json object to a file
-f2 = open("Parks_Data.json", "w")
-json.dump(dictionary, f2, indent = 4)
+f2 = open("Data_Journalism/data/Parks_Data.json", "w")
+json.dump(list, f2, indent = 4)
 
 f2.close()
