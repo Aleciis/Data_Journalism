0b0034d45f87dccbf52b77beb08e03412d266b48
Alexis Curisaca <alexis.curisaca25@trinityschoolnyc.org>
2024-04-23 23:28:22 -0400

    milestone 1 complete?

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # flask --app data_server run
 from flask import Flask
 from flask import render_template
+from flask import request
 import json
 
 
@@ -12,15 +13,48 @@ def year():
     data=json.load(f)
     f.close()
     print(data)
-    return render_template('macro_page.html', Parks= data.keys())
+    Parks= data.keys()
+    Parks_Underscored= []
+    for key in Parks:
+        key = key.replace(" ", "_")
+        Parks_Underscored.append(key)
+    #print(Parks_underscored)
+    return render_template('macro_page.html', Parks= data.keys(), Parks_underscored=Parks_Underscored, Parks_and_Parks_Underscored=zip(Parks,Parks_Underscored))
 
 @app.route('/about')
 def index():
     f= open("Data_Journalism/data/Parks_Data.json", "r")
     data=json.load(f)
     f.close()
-    print(data)
-    return render_template('about.html',  Parks= data.keys())
+    #print(data)
+    Parks= data.keys()
+    Parks_Underscored= []
+    for key in Parks:
+        key = key.replace(" ", "_")
+        Parks_Underscored.append(key)
+    #print(Parks_underscored)
+    
+    return render_template('about.html',  Parks= data.keys(), Parks_Underscored=Parks_Underscored, Parks_and_Parks_Underscored=zip(Parks,Parks_Underscored) )
+
+@app.route('/micro')
+def micro():
+    f= open("Data_Journalism/data/Parks_Data.json", "r")
+    data=json.load(f)
+    f.close()
+    print(f"request.url={request.url}")
+    print(f"request.url={request.query_string}")
+    Individual_Park=request.query_string.decode()
+    Individual_Park_Spaces=Individual_Park.replace("_", " ")
+    print(Individual_Park_Spaces)
+    Parks= data.keys()
+    Parks_Underscored= []
+    for key in Parks:
+        key = key.replace(" ", "_")
+        Parks_Underscored.append(key)
+    #print(Parks_underscored)
+    
+    
+    return render_template('micro_page.html',  Parks= data.keys(), Parks_underscored=Parks_Underscored, Parks_and_Parks_Underscored=zip(Parks,Parks_Underscored), Individual_Park_Spaces = Individual_Park_Spaces)
 
 
     
