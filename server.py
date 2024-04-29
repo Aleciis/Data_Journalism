5a64424c7b1fa1d08cc84c83278d1f0d04a363c7
Alexis Curisaca <alexis.curisaca25@trinityschoolnyc.org>
2024-04-28 23:27:59 -0400

    needs SVG interactions

--- server.py
+++ server.py
@@ -45,18 +45,34 @@ def micro():
     print(f"request.url={request.query_string}")
     Individual_Park=request.query_string.decode()
     Individual_Park_Spaces=Individual_Park.replace("_", " ")
-    #print(Individual_Park_Spaces)
+    Individual_Park_Spaces_decoded=Individual_Park_Spaces.replace("%27", "'")
+    print(Individual_Park_Spaces_decoded)
     Parks= data.keys()
     Parks_Underscored= []
     for key in Parks:
         key = key.replace(" ", "_")
         Parks_Underscored.append(key)
-    Individual_Park_Spaces_String = str(Individual_Park_Spaces)
-    print(data[Individual_Park_Spaces_String]["ACRES"])
+    Individual_Park_Spaces_String = str(Individual_Park_Spaces_decoded)
+    #print(Individual_Park_Spaces)
+    #print(data[Individual_Park_Spaces_String]["BOROUGH"])
+
+    for borough in data[Individual_Park_Spaces_String]["BOROUGH"]:
+        if str(borough) == "M":
+            borough = "Manhattan"
+        if str(borough) == "B":
+            borough = "Brooklyn"
+        if str(borough) == "X":
+            borough = "The Bronx"
+        if str(borough) == "Q":
+            borough = "Queens"
+        if str(borough) == "R":
+            borough = "Staten Island"
+    print(borough)
+    print(data[Individual_Park_Spaces_String])
     #print(Parks_underscored)
     
     
-    return render_template('micro_page.html',  Parks= data.keys(), Parks_Underscored=Parks_Underscored, Parks_and_Parks_Underscored=zip(Parks,Parks_Underscored), Individual_Park_Spaces = Individual_Park_Spaces, Park_Acres = data[Individual_Park_Spaces_String]["ACRES"])
+    return render_template('micro_page.html',  Parks= data.keys(), Parks_Underscored=Parks_Underscored, Parks_and_Parks_Underscored=zip(Parks,Parks_Underscored), Individual_Park_Spaces = Individual_Park_Spaces_String, Park_Acres = data[Individual_Park_Spaces_String]["ACRES"],Park_Borough = borough,Park_Location = data[Individual_Park_Spaces_String]["LOCATION"],Park_Type = data[Individual_Park_Spaces_String]["TYPECATEGORY"],Park_URL = data[Individual_Park_Spaces_String]["URL"],Park_Zipcode = data[Individual_Park_Spaces_String]["ZIPCODE"])
 
 
     
