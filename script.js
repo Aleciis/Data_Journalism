a28f3c5c2a2675eda49627cbe2219a36f82fa9bf
Aleciis <alexis.curisaca25@trinityschoolnyc.org>
2024-04-29 12:06:41 -0400

    still need svg implementation :(

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var park = document.getElementById()
+var park = document.getElementById("path")
 // clicking on each individual svg will send you to a different sub-macro page showing an svg of the borough and every park within that borough
 // using id in each svg (each id is a different zipcode), can be used as a query string for each sub-macro page
 // is it possible to have a different page for each svg??
